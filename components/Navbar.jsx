3f5fa76d3467dd424121199e25ef7209f703e291
John Doe <johndoe@email.com>
2023-02-07 23:09:49 +0600

    Last commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,37 +1,48 @@
+"use client"
 import Link from 'next/link'
+import { useState } from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 function Navbar() {
+
+    const [nav, setNav] = useState(false)
     
   return (
     <div className='fixed top-0 left-0 w-full z-10 ease-in bg-[#fefeff]'>
         <div className='max-w-[1240px] m-auto flex justify-between items-center p-4'>
             <Link href='/'>
-                <h1 className='font-bold'>Gym <span/> baran </h1>
+                <div className='text-2xl flex p-4'>
+                    <h1 className='p-1 bg-[#6765f0] object-cover object-center rounded text-white'>
+                    Gym 
+                    </h1>
+                    <h1 className='p-1 text-[#6765f0]'>
+                    baran
+                    </h1>
+                </div>
             </Link>
             <ul className='hidden sm:flex'>
-                <li className='p-4'>
+                <li className='p-4 text-xl'>
                     <Link href='/'>Home</Link>
                 </li>
-                <li className='p-4'>
+                <li className='p-4 text-xl'>
                     <Link href='/'>Program</Link>
                 </li>
-                <li className='p-4'>
+                <li className='p-4 text-xl'>
                     <Link href='/'>Blog</Link>
                 </li>
-                <li className='p-4'>
+                <li className='p-4  text-xl'>
                     <Link href='/'>About us</Link>
                 </li>
-                <li className='p-4'>
+                <li className='p-4 w-24 text-xl bg-[#274374] text-center object-cover object-center rounded-xl text-white'>
                     <button href='/'>Login</button>
                 </li>
             </ul>
 
             <div className='block sm:hidden z-10'>
-            <AiOutlineMenu size={20}/>
+            {nav? <AiOutlineClose onClick={()=> setNav(false) } size={30}/> : <AiOutlineMenu onClick={()=> setNav(true) }  size={30}/>}
         </div>
 
-        <div className='sm:hidden absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center w-full h-screen bg-white ease-in'>
+        <div className={nav? 'sm:hidden absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center w-full h-screen bg-white ease-in': 'hidden'}>
         <ul>
                 <li className='p-4 text-3xl'>
                     <Link href='/'>Home</Link>
